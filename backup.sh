dcafbd77bfc83f8b65474b683e7c461c84e711d9
fmeineke <frank.meineke@imise.uni-leipzig.de>
2024-02-08 18:16:21 +0100

    fixed wrong backup name

--- backup.sh
+++ backup.sh
@@ -5,10 +5,11 @@ source .env
 source docker-compose.env
 
 BACKUPDIR=backup
-NOW=$(date +'%Y-%m-%d_%H:%M:%S')
-DB_BACKUP=$BACKUPDIR/${COMPOSE_PROJECT_NAME}-db-$NOW.sql.gz
+# dont use : in time
+NOW=$(date +'%Y-%m-%dT%H-%M-%S')
+DB_BACKUP=${COMPOSE_PROJECT_NAME}-db-$NOW.sql.gz
 DB_COPIES=14
-FILE_BACKUP=$BACKUPDIR/${COMPOSE_PROJECT_NAME}-filestore-$NOW.tar.gz
+FILE_BACKUP=${COMPOSE_PROJECT_NAME}-filestore-$NOW.tar.gz
 FILE_COPIES=2
 
 
@@ -18,14 +19,14 @@ docker-compose exec -T db mysqldump \
 	--user=root \
 	--password=$MYSQL_ROOT_PASSWORD \
 	--host=$MYSQL_HOST $MYSQL_DATABASE \
-	| tee 2>$BACKUPDIR/error.log | gzip -c >$DB_BACKUP
+	| tee 2>$BACKUPDIR/error.log | gzip -c >$BACKUPDIR/$DB_BACKUP
 
-docker-compose exec -T seek tar cfz - -C / seek/filestore  > $FILE_BACKUP
+docker-compose exec -T seek tar cfz - -C / seek/filestore  > $BACKUPDIR/$FILE_BACKUP
 find $BACKUPDIR -type f -name '*.sql.gz' -mtime +${DB_COPIES} -exec rm {} \;
 find $BACKUPDIR -type f -name '*.tar.gz' -mtime +${FILE_COPIES} -exec rm {} \;
 
-(cd $BACKUPDIR && ln -sf database-$NOW.sql.gz  database-current.sql.gz)
-(cd $BACKUPDIR && ln -sf filestore-$NOW.tar.gz  filestore-current.tar.gz)
+(cd $BACKUPDIR && ln -sf $DB_BACKUP  database.sql.gz)
+(cd $BACKUPDIR && ln -sf $FILE_BACKUP  filestore.tar.gz)
 
 
-ls -l $FILE_BACKUP $DB_BACKUP
+ls -l $BACKUPDIR/$FILE_BACKUP $BACKUPDIR/$DB_BACKUP
