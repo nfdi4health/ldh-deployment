35c7f55e2482bec38db6262f9b1042ad4de60c23
fmeineke <frank.meineke@imise.uni-leipzig.de>
2024-02-08 15:08:28 +0100

    removed use of backupdir; just use database and filestore backups

--- restore.sh
+++ restore.sh
@@ -5,8 +5,6 @@
 source .env
 source docker-compose.env
 
-BACKUPDIR=backup
-
 usage() {
     echo -e "Usage: $0 database.sql.gz filestore.tar.gz"
 	echo filestore would be restored from $FILE_BACKUP
@@ -22,13 +20,11 @@ fi
 DB_BACKUP=$1
 FILE_BACKUP=$2
 
-mkdir -p $BACKUPDIR
-
 echo restore database
 gzip -cd ${DB_BACKUP}| docker-compose exec -T db mysql -u seek --password=$MYSQL_PASSWORD seek
 
 echo restore filestore
-docker-compose cp backup/filestore.tar.gz seek:/seek
+docker-compose cp ${FILE_BACKUP} seek:/seek/filestore.tar.gz
 docker-compose exec seek tar xfz filestore.tar.gz -C / 
 docker-compose exec seek rm -f filestore.tar.gz
 
