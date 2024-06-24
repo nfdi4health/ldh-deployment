5b85632645a98c9ff37a612bf8ba2956146ebc9e
Frank Meineke <frank.meineke@imise.uni-leipzig.de>
2024-06-24 13:24:02 +0200

    compose as option

--- start.sh
+++ start.sh
@@ -4,6 +4,6 @@ source .env
 
 docker volume create ${COMPOSE_PROJECT_NAME}_db
 docker volume create ${COMPOSE_PROJECT_NAME}_filestore
-docker-compose up -d
-docker-compose logs -f --tail 1
+docker compose up -d
+docker compose logs -f --tail 1
  
