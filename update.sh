3996561fc98d57b2577e9180c7e42ea3d57b480e
Frank Meineke <frank.meineke@imise.uni-leipzig.de>
2024-06-24 13:26:09 +0200

    compose option

--- update.sh
+++ update.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-docker-compose down
-docker-compose pull
+# if images have changed or new release 
+docker compose down
+docker compose pull
 # avoid the seek-workers, which will interfere
-docker-compose up -d seek db solr   
-docker-compose exec seek docker/upgrade.sh 
-docker-compose down
-docker-compose up -d
+docker compose up -d seek db solr   
+docker compose exec seek docker/upgrade.sh 
+docker compose down
+docker compose up -d
  
